f7d89a4f39cb519922b82c5fcdc5d887f568e999
Martin Ben <mebenav88@gmail.com>
2016-03-05 15:28:11 -0500

    create course model

--- APICourse/courses/models.py
+++ APICourse/courses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    created = models.DateTimeField(auto_now_add = True)
+    title = models.CharField(max_length = 100, blank = True, default = "")
+    description = models.TextField()
+    owner = models.ForeignKey('auth.User', related_name = 'courses')
+
+    class Meta:
+        ordering = ('created',)
